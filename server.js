9cbf05b83bbe00c98aedc9eca1348acf0bca615c
LexaMart <lexamart1708@gmail.com>
2022-09-01 11:21:56 +0300

    add dotenv library

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const path = require("path");
-const tgInfo = require("./secretKey");
+// const tgInfo = require("./secretKey");
+require("dotenv").config();
 const TelegramApi = require("node-telegram-bot-api");
 
 const app = express();
+const botToken = process.env.BOT_TOKEN || null;
+const chatId = process.env.CHAT_ID || null;
 const PORT = process.env.PORT || 8080;
-const bot = new TelegramApi(tgInfo.botToken, { polling: true });
-bot.on('invoice', msg => {
-    console.log(msg);
-})
+// const bot = new TelegramApi(tgInfo.botToken, { polling: true });
+const bot = new TelegramApi(botToken, { polling: true });
+
+bot.on("invoice", (msg) => {
+  console.log(msg);
+});
 
 app.use(express.static(path.join(__dirname, "build")));
 app.use(express.json());
@@ -20,11 +25,15 @@ app.get("/*", (req, res) => {
 
 app.post("/submit", (req, res) => {
   const form = req.body;
-  bot.sendMessage(tgInfo.chatId, `
+  bot.sendMessage(
+    chatId,
+    `
   *НОВАЯ ЗАЯВКА!*\n\n\n*ИМЯ КЛИЕНТА:* ${form.name}\n\n*ТЕЛЕФОН*: ${form.phone}\n\n*ОСТАВЛЕННОЕ СООБЩЕНИЕ: *${form.message}\n\n*НАЗВАНИЕ ПРОДУКТА: *${form.product}\n
-  `,{
-    parse_mode: 'Markdown'
-  })
+  `,
+    {
+      parse_mode: "Markdown",
+    }
+  );
   res.sendStatus(200);
 });
 
