8af3d4584db41c9204cb58baee4385854d7a931d
zhenekq <zhenek02ss@gmail.com>
2022-10-23 19:46:48 +0400

    add loader

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -3,14 +3,19 @@ import { Outlet } from "react-router-dom";
 import { FeedBackForm } from "../feedBackForm";
 import { Footer } from "../footer";
 import { Header } from "../header";
+import { Loader } from "../loader";
 
 export const Layout = ({ ...props }) => {
   return (
+    
     <>
       <FeedBackForm />
       <Header />
       <Outlet />
       <Footer />
     </>
+    /*<>
+      <Loader />
+    </>*/
   );
 };
